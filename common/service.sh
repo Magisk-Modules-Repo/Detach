ec38b999b37f5b5dc9aed12d16b7d21b92e1bc91
Rom <skav@gmx.fr>
2019-03-30 01:56:37 +0100

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,29 +1,31 @@
 #!/system/bin/sh
-# Please don't hardcode /magisk/modname/... ; instead, please use $MODDIR/...
-# This will make your scripts compatible even if Magisk change its mount point in the future
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-# This script will be executed in late_start mode
+# This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
 # Detach Apps from Market by hinxnz
 
 # Playstore database and SQLite directory'
 PLAY_DB_DIR=/data/data/com.android.vending/databases
-SQLITE=$MODDIR
+SQLITE=$MODDIR/sqlite
 
 # Wait till boot has completed'
 (while [ 1 ]; do
 	if [ `getprop sys.boot_completed` = 1 ]; then sleep 60
 			
 # Disable service that populates database
-		pm disable com.android.vending/com.google.android.finsky.dailyhygiene.DailyHygiene'$'DailyHygieneService
-		pm disable com.android.vending/com.google.android.finsky.hygiene.DailyHygiene'$'DailyHygieneService
+# (in investigation..)
 	
 # Stop playstore to make changes
 		am force-stop com.android.vending
 
 # Change directory'
-		cd $SQLITE
+		cd $MODDIR
 
 # Detach following apps from market
