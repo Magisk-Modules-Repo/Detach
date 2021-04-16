5edd9e7db58b70fbd62ad296f4a2b7e3559d1749
sobuj53 <25761237+sobuj53@users.noreply.github.com>
2021-04-16 15:43:02 +0600

    Add files via upload

--- service.sh
+++ service.sh
@@ -4,26 +4,24 @@
 # This will make sure your module will still work if Magisk change its mount point in the future
 
 MODDIR=${0%/*}
+
 # This script will be executed in late_start service mode. More info in the main Magisk thread
 # Detach Apps from Market by hinxnz
+
 
 MAGMOD=/data/adb/modules/Detach
 
 #Let boot status 
-i=0
-# Wait till boot has completed'
-(while [ 1 ]; do
 
-	
-	sleep 5
+# Wait till boot has completed'
+(while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 20; done
+
 
 	if [ `getprop sys.boot_completed` = 1 ]; then 
 	
-		((i++))
-		if [[ $i -gt 1 ]]; then
-			break
-		fi
+	
 		#Reserved for crond schedule
+		crond -b -c /data/adb/modules/Detach/crons -L /storage/emulated/0/cronlogs.txt
 		
 		
 		#prevent recursive run
@@ -31,4 +29,4 @@ i=0
 		
 	fi
 	
-done &)
+)
